9455510e7824c6aa1676fcdea76d282a13950089
Rjviviers <Rjviviers@gmail.com>
2020-09-17 15:39:37 +0200

    debug again

--- html_private/conf.php
+++ html_private/conf.php
@@ -3,7 +3,7 @@
 
 define('CLIENT_ID', '15eb0efcd4b64909a462e68c8a34ff66');
 define('CLIENT_SECRET', '9729a66cde744abaa4ba190d6424b3ee');
-define('REDIRECT_URI', 'https://www.rwdmusic.co.za/app.php');
+define('REDIRECT_URI', 'https://www.rwdmusic.co.za/callback.php');
 
 define("DB_TYPE", "mysql");
 define("DB_HOST", "localhost");
