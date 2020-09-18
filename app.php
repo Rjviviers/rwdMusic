b77cbd85e199eb318262c736ef11f82a354e70dc
Rjviviers <Rjviviers@gmail.com>
2020-09-18 15:29:32 +0200

    s

--- app.php
+++ app.php
@@ -9,13 +9,13 @@ $api->setAccessToken($_SESSION['accessToken']);
 // It's now possible to request data about the currently authenticated user
 $track = $api->getTrack('4wkNbCituY2LtDKRbgoqH5');
 
-echo '<b>' . $track->name . '</b> by <b>' . $track->artists[0]->name . '</b>';
+echo '<b>' . $track->name . '</b> by <b>' . $track->artists[0]->name . '</b><br>';
 $imgsrc = $track->album->images[0]->url;
 echo "<img src= '$imgsrc' >  ";
-echo "</br> images atr";
-foreach ($track->album->images as $key => $value) {
-    echo "<br> ". $key . " : " . print_r($value) . "</br>";
-}
+// echo "</br> images atr";
+// foreach ($track->album->images as $key => $value) {
+//     echo "<br> ". $key . " : " . print_r($value) . "</br>";
+// }
 
 // echo "</br> trac atr";
 // foreach ($track as $key => $value) {
