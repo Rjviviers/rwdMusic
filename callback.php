a7e6f2526017a1ee6c77ac1ff6356ca1b72fc403
Rjviviers <Rjviviers@gmail.com>
2020-09-18 10:57:28 +0200

    callback fix

--- callback.php
+++ callback.php
@@ -19,6 +19,24 @@ $refreshToken = $session->getRefreshToken();
 $_SESSION['accessToken'] = $accessToken;
 $_SESSION['refreshToken'] = $refreshToken;
 var_dump($_SESSION);
+
+
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+var_dump($_SESSION);
+// Fetch the saved access token from somewhere. A database for example.
+//$api->setAccessToken($_SESSION['accessToken']);
+
+// It's now possible to request data about the currently authenticated user
+print_r(
+    $api->me()
+);
+
+// Getting Spotify catalog data is of course also possible
+print_r(
+    $api->getTrack('7EjyzZcbLxW7PaaLua9Ksb')
+);
+
+
 // Send the user along and fetch some data!
 $myConn->redirect("app.php");
 die();
