019f7c2912be1c121926cd6d2c29959aa74c337a
Rjviviers <Rjviviers@gmail.com>
2020-09-18 15:35:46 +0200

    form

--- app.php
+++ app.php
@@ -1,17 +1,31 @@
 <?php
 require 'vendor/autoload.php';
 require('html_private/head.php');
+include __DIR__ . '/partials/header.php';
+
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 
 // Fetch the saved access token from somewhere. A database for example.
 $api->setAccessToken($_SESSION['accessToken']);
-
+if (isset($_GET['go'])) {
+    $track = $api->getTrack($_GET['song']);
+    echo '<b>' . $track->name . '</b> by <b>' . $track->artists[0]->name . '</b><br>';
+    $imgsrc = $track->album->images[0]->url;
+    echo "<img src= '$imgsrc' >  ";
+}
 // It's now possible to request data about the currently authenticated user
-$track = $api->getTrack('4wkNbCituY2LtDKRbgoqH5');
 
-echo '<b>' . $track->name . '</b> by <b>' . $track->artists[0]->name . '</b><br>';
-$imgsrc = $track->album->images[0]->url;
-echo "<img src= '$imgsrc' >  ";
+?>
+
+
+<form method="get">
+    <input type="text" name="song">
+    <input type="submit" value="go">
+</form>
+
+
+<?php
+
 // echo "</br> images atr";
 // foreach ($track->album->images as $key => $value) {
 //     echo "<br> ". $key . " : " . print_r($value) . "</br>";
