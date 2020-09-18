2c16bfc41f7a398167134838c8f7768cde09fe5e
Rjviviers <Rjviviers@gmail.com>
2020-09-18 12:46:10 +0200

    track vardump

--- app.php
+++ app.php
@@ -10,10 +10,7 @@ $api->setAccessToken($_SESSION['accessToken']);
 $track = $api->getTrack('4wkNbCituY2LtDKRbgoqH5');
 
 echo '<b>' . $track->name . '</b> by <b>' . $track->artists[0]->name . '</b>';
-
-$analysis = $api->getAudioAnalysis('4wkNbCituY2LtDKRbgoqH5');
-
-print_r($analysis);
+var_dump($track);
 
 // foreach ($api->me() as $key => $value) {
 //     echo $key . " : " . print_r($value) ."</br>";
