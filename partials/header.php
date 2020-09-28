4e3afaef4f2e3a622dfd02f2457f102ab5328ebc
Rjviviers <Rjviviers@gmail.com>
2020-09-28 13:46:38 +0200

    color

--- partials/header.php
+++ partials/header.php
@@ -52,10 +52,10 @@ function toggle() {
     </nav>
 
 
-    <div class="modal fade" id="sngForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade " id="sngForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
         <div class="modal-dialog" role="document">
-            <div class="modal-content">
+            <div class="modal-content bg-dark">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Song add method</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
