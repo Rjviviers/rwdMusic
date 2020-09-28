0df757aa8ef761e4381422bb8a92ccb0e8ddcffc
Rjviviers <Rjviviers@gmail.com>
2020-09-28 13:42:39 +0200

    add song replace

--- partials/header.php
+++ partials/header.php
@@ -40,7 +40,9 @@ function toggle() {
                     <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item">
-                    <button class="nav-link btn" onclick="toggle()" href="">Add Song</button>
+                    <!-- <button class="nav-link btn" onclick="toggle()" href="">Add Song</button> -->
+                    <button type="button" class="nav-link btn" data-toggle="modal" data-target="#sngForm">Add
+                        Song</button>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link " href="logout.php">Log out</a>
@@ -49,18 +51,48 @@ function toggle() {
         </div>
     </nav>
 
+
+    <div class="modal fade" id="sngForm" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Song add method</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nu">Number of songs to add</label>
+                        <div class="row pl-3 row-cols-4">
+                            <input id="nu" class="form-control col-xl" type="number" name="quantity" min="1" max="15"
+                                value="1">
+                            <button class="pt-1 pl-2 btn btn-warning col" onclick="addsong()">GO</button>
+                        </div>
+                        <!-- <input type="submit" class="btn btn-secondary form-control" name="go" value="go"> -->
+                    </div>
+                    <div class="col pt-2 pl-3 row-cols-1">
+
+                        <?php if (!empty($_COOKIE['spotify'])) { ?>
+                        <label class="col">mag dalk nogi werki lel</label>
+                        <a class="btn col btn-warning" href="addsong.php?s=yes">
+                            spotify song add
+                        </a>
+                        <?php } ?>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div id="songfrm" class="hide">
         <div class="row row-cols-2">
             <div class="col pt-2 pl-4">
-                <div class="form-group">
-                    <label for="nu">Number of songs to add</label>
-                    <div class="row pl-3 row-cols-4">
-                        <input id="nu" class="form-control col-xl" type="number" name="quantity" min="1" max="15"
-                            value="1">
-                        <button class="pt-1 pl-2 btn btn-warning col" onclick="addsong()">GO</button>
-                    </div>
-                    <!-- <input type="submit" class="btn btn-secondary form-control" name="go" value="go"> -->
-                </div>
+
                 <!-- <div class="form-group">
                     <label for="nu">Number of songs to add</label>
                     <input id="nu" class="form-control" type="number" name="quantity" min="1" max="15" value="1">
@@ -69,15 +101,7 @@ function toggle() {
                 </div> -->
 
             </div>
-            <div class="col pt-2 pl-3 row-cols-1">
 
-                <?php if (!empty($_COOKIE['spotify'])) { ?>
-                <label class="col">mag dalk nogi werki lel</label>
-                <a class="btn col btn-warning" href="addsong.php?s=yes">
-                    spotify song add
-                </a>
-                <?php } ?>
-            </div>
         </div>
 
     </div>
